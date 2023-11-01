96d7e6dda5b077e65adc4d8f6c76ee2ef0441015
RayFigueiredo <147215305+RayFigueiredo@users.noreply.github.com>
2023-10-31 21:45:41 -0300

    Update Models.py

--- ProjetoCRUD/Models.py
+++ ProjetoCRUD/Models.py
@@ -1,24 +1,24 @@
 import mysql.connector
 
 # CONEXÃO COM BD LOCAL MYSQL
-conexao = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='Mack#2351',
-    database='bdferias'
-)
-cursor = conexao.cursor()
+# conexao = mysql.connector.connect(
+#     host='localhost',
+#     user='root',
+#     password='Mack#2351',
+#     database='bdferias'
+# )
+# cursor = conexao.cursor()
 
 # CONEXÃO COM BD MY SQL NA NUVEM SERVIDOR RAILWAY - FERIAS
 
-# conexao = mysql.connector.connect(
-#     host='containers-us-west-87.railway.app',
-#     user='root',
-#     password='945bo1bQFfqgqWY3NtLH',
-#     database='railway',
-#     port='6810'
-# )
-# cursor = conexao.cursor()
+conexao = mysql.connector.connect(
+    host='containers-us-west-87.railway.app',
+    user='root',
+    password='945bo1bQFfqgqWY3NtLH',
+    database='railway',
+    port='6810'
+)
+cursor = conexao.cursor()
 
 cursor.close()
 conexao.close()
